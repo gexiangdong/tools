1fee7e5f1f32fdc3f91eaaaa3bfb278905621a9c
gexiangdong <gexiangdong@users.noreply.github.com>
2015-04-16 10:47:58 +0800

    Update package.py

--- package.py
+++ package.py
@@ -14,34 +14,63 @@ import io
 import logging
 import re
 import codecs
+import cgi
 import subprocess
+import sys
+from datetime import datetime
 from os import path
 from html.parser import HTMLParser
 from webassets import Bundle, Environment
 
-outFile = codecs.open("/home/gexiangdong/out.txt", 'w', 'utf-8')
-returnCode = subprocess.call(['/web/git/deploy.sh', ''], stdout=outFile);
-outFile.close();
-print(returnCode)
+print("Content-type:text/plain")
+print("")
 
 
-version = "wx20150310"
-zipFile = "web.zip";
-projectFolder = "/Users/gexiangdong/git/saofenbao/saofenbaoweixin" 
-tempFolder = "/Users/gexiangdong/temp"
+version = ""
+form = cgi.FieldStorage()
+if(form.getvalue("version") is None):
+    version="prerelease"
+else:
+    version=form.getvalue("version")
+
+
+scriptPath=""
+if("SCRIPT_FILENAME" in os.environ.keys()):
+    scriptPath=os.environ["SCRIPT_FILENAME"]
+else:
+    scriptPath="/web/htdocs/projects/package.py"
+
+scriptFolder = os.path.dirname(scriptPath)
+
+outFile = codecs.open(os.path.join(scriptFolder, "output/out.txt"), 'w', 'utf-8')
+returnCode = subprocess.call(['/web/temp/build.sh', version], stdout=outFile);
+if(returnCode == 0):
+    outFile.write("\n\nbuild done.\npacking the zip file....\n\n")
+    #outFile.close();
+else:
+    print('{"status":1, "errorMessage":"error while building package, see build log for detail. ' + str(returnCode) + '"}')
+    outFile.close()
+    sys.exit()
+    
+
+
+zipFile = "output/web-" + version + "." + datetime.now().strftime("%Y%m%d%H%M%S") + ".zip";
+projectFolder = "/web/temp/saofenbao/saofenbaoweixin" 
+tempFolder = os.path.join(scriptFolder, "temp")
+#if(!os.path.exists(tempFOlder)):
+   
 #"/web/git/saofenbao/saofenbaoweixin/"
 
 #不需要打包进入的文件
 excludes = ['DS_Store', 'gitignore', 'files', 'WEB-INF/autostart.xml', 'hd/*', 'ceshi.jsp', 'cookie.jsp', 'dbinfo.jsp', 'test.jsp', 'mlogout.jsp', 'META-INF', 'WEB-INF/lib/(?!clschina).*.jar']
 
 #需要压缩的CSS和JS，不仅仅压缩还会搜寻templetFile来更改里面引用的链接URL
-#css文件必须在css目录下；js文件必须在js目录下，其他目录不处理
 cssFiles = ['css/main.css', 'css/addition.css']
 jsFiles = ['js/sfb.js']
 templetFiles = ['templet/layout/default.shtml']
 contextPath = '${request.contextPath}/'
 
-zf = zipfile.ZipFile(zipFile, 'w')
+zf = zipfile.ZipFile(os.path.join(scriptFolder, zipFile), 'w')
 
 zf.writestr("version.txt", version)
 
@@ -52,30 +81,33 @@ def addFolderToZip(folder):
     dirList = os.listdir(folder)
     for fileName in dirList:
         filePath = os.path.join(folder, fileName)
+        #outFile.write("checking " + filePath + "\n")
         if os.path.isdir(filePath):
             addFolderToZip(filePath)
         else:
             relatedPath = filePath[len(webFolder)+1:]
+            outFile.write("checking " + relatedPath + "\n")
             if relatedPath in cssFiles:
-                #print(relatedPath, "CSS file...")
-                break
+                outFile.write("  temporary omit " + relatedPath + " (CSS file).\n")
+                continue
             if relatedPath in jsFiles:
-                #print(relatedPath, "JS file ...")
-                break
+                outFile.write("  temporary omit " + relatedPath + " (JS file).\n")
+                continue
             if relatedPath in templetFiles:
-                #print(relatedPath, "Templet file ...")
-                break
+                outFile.write("  temporay omit " + relatedPath + " (Layout Templet file)\n")
+                continue
                 
             shouldBeInclude = True
             for pattern in excludes:
                 #print(pattern, " -- ", relatedPath)
                 if(re.search(pattern, relatedPath) is not None):
                     shouldBeInclude = False
-                    #print(relatedPath, " omit.... (" + pattern + " matched.)")
-                    break
+                    outFile.write("  omit " + relatedPath + " (" + pattern + " matched.)")
+                    continue
             
             if(shouldBeInclude):
                 zf.write(filePath, relatedPath)
+                outFile.write("  add " + relatedPath + "\n")
             
 addFolderToZip(webFolder)
 
@@ -104,6 +136,7 @@ def compressFile(f, webFilters):
     
 
 #处理CSS
+outFile.write("compress CSS files....\n")
 cssFileVersions = {}
 for cssFile in cssFiles:
     f = os.path.join(webFolder, cssFile)
@@ -114,6 +147,7 @@ for cssFile in cssFiles:
 
     
 #处理JS
+outFile.write("compress JS files....\n")
 jsFileVersions = {}
 for jsFile in jsFiles:
     f = os.path.join(webFolder, jsFile)
@@ -123,7 +157,7 @@ for jsFile in jsFiles:
         jsFileVersions[jsFile]="error"
 
 #处理Templet
-
+outFile.write("update layout templet file.\n")
 class LayoutHTMLParser(HTMLParser):
     
     def __init__(self):
@@ -149,11 +183,10 @@ class LayoutHTMLParser(HTMLParser):
     def replacement(self):
         return self.__replacement
  
-#print("OK")
 for templet in templetFiles:
     parser = LayoutHTMLParser()
-    fh = open(os.path.join(webFolder, templet), 'r')
-    t = (fh.read())
+    fh = open(os.path.join(webFolder, templet), mode='r', encoding='utf-8')
+    t = fh.read()
     fh.close()
     parser.feed(t)
     replacement = parser.replacement()
@@ -164,6 +197,8 @@ for templet in templetFiles:
 
 
 zf.close()
+outFile.write("\nZip file created.\n")
+outFile.close()
 
-
+print('{"status":0, "errorMessage":"success.", "file":"' +  zipFile + '"}')
     
